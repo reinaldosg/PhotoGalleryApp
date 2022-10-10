e27128677bb9f59fb0196d568d465c9db0331f14
Reinaldo S.G <reinaldogunawan@yahoo.com>
2022-10-11 02:39:23 +0700

    fix test

--- app/src/test/java/com/scrapps/galleryapplication/presentation/listphotos/ListPhotosViewModelTest.kt
+++ app/src/test/java/com/scrapps/galleryapplication/presentation/listphotos/ListPhotosViewModelTest.kt
@@ -1,27 +1,27 @@
 package com.scrapps.galleryapplication.presentation.listphotos
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.paging.PagedList
 import com.scrapps.galleryapplication.*
 import com.scrapps.galleryapplication.data.remote.UnsplashApi
 import com.scrapps.galleryapplication.data.remote.entity.PhotosResponse
 import com.scrapps.galleryapplication.data.remote.entity.PhotosUrl
 import com.scrapps.galleryapplication.data.remote.entity.PhotosUser
-import com.scrapps.galleryapplication.domain.model.PhotoModel
-import com.scrapps.galleryapplication.domain.source.LoadPhotoSource
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runners.JUnit4
-import retrofit2.Call
+import retrofit2.*
+
 class ListPhotosViewModelTest : UnitTest() {
     @get:Rule
     val rule = InstantTaskExecutorRule()
     private lateinit var listPhotosViewModel: ListPhotosViewModel
+
 
     @MockK
     private lateinit var service: UnsplashApi
@@ -33,7 +33,7 @@ class ListPhotosViewModelTest : UnitTest() {
 
     @Test
     fun `loading photos should update live data`() {
-        val photoList = listOf(
+        val listPhoto = listOf(
             PhotosResponse(
             "VvhC97pYcUQ",
                 "this is description",
@@ -59,14 +59,22 @@ class ListPhotosViewModelTest : UnitTest() {
                 PhotosUser("Madison Kuhn")
             )
         )
-        coEvery { service.loadPhotos(any(), any(), any()) } returns MockCall.buildSuccess(photoList)
-        runBlocking { listPhotosViewModel.getListPhoto() }
-        Assert.assertEquals(2, listPhotosViewModel.listPhotos.getOrAwaitValue().size)
+        val mockedCall = mockk<Call<List<PhotosResponse>>>(relaxed = true)
+        every { mockedCall.enqueue(any()) } answers {
+            //Get the callback from the arguments
+            val callback = args[0] as Callback<List<PhotosResponse>>
 
-//        listPhotosViewModel.listPhotos.observeForever {
-//            Assert.assertEquals(it.size,0)
-//            Assert.assertEquals(it[0]?.description, "this is description")
-//            Assert.assertNull(it[1]?.description)
-//        }
+            callback.onResponse(mockedCall, retrofit2.Response.success(listPhoto))
+        }
+        coEvery { service.loadPhotos(any(), any(), any()) } returns mockedCall
+
+        runBlocking { listPhotosViewModel.getListPhoto() }
+        Assert.assertNotNull("should not be null", listPhotosViewModel.listPhotos.getOrAwaitValue().size)
+
+        listPhotosViewModel.listPhotos.observeForever {
+            Assert.assertEquals(2,it.size)
+            Assert.assertEquals("this is description", it[0]?.description)
+            Assert.assertNull(it[1]?.description)
+        }
     }
 }
